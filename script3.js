6ff7b47c8eac6eb02db8b2b87b5c649d7dec7e1e
Doghmane Daoud <doghmane.daoud@gmail.com>
2020-06-18 15:44:29 +0200

    Manipuler les styles css

---
+++ script3.js
@@ -0,0 +1,10 @@
+/*Rappel sur les notations :
+Camel case : backgroundColor
+Train case : background-color */
+
+//Modifier un style css (utilisation Camel case)
+window.onload = function(){
+    var le_paragraphe = document.getElementsById("le_paragraphe");
+    //backgroun-colorr
+    le_paragraphe.style.backgroundColor = 'black';
+}
