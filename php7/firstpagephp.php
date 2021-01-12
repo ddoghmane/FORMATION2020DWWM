8132381af1a5164d2134239dcb8dac5efc252806
Doghmane Daoud <doghmane.daoud@gmail.com>
2021-01-12 21:32:24 +0100

    ajout colorpicker

---
+++ php7/firstpagephp.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html> <html lang="fr">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> <title>Une page PHP</title>
+</head> <body> <?php
+echo "<h3> Aujourd'hui le ". date('d / M / Y H:m:s')."</h3><hr />";
+echo "<h2>Bienvenue sur le site PHP 7</h2>"; ?>
+</body> </html>
