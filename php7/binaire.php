6da6c2b449d47a9f7cdab2a95fa767bc4e6209e7
Doghmane Daoud <doghmane.daoud@gmail.com>
2021-02-03 11:22:37 +0100

    Ajout caractere formatage du type de donnée

---
+++ php7/binaire.php
@@ -0,0 +1,17 @@
+<?php $ch = "89";
+printf("En binaire $ch s'écrit %b <br />", $ch);
+$ch = "115";
+printf(" Le caractère de code $ch est %c <br />", $ch);
+
+$ch = "-25";
+printf("La valeur de \$ch est %d <br/>", $ch);
+
+$ch = "25.52 mètres";
+printf("La longueur est de %f m  <br/>", $ch);
+
+$ch = 252;
+printf ("En octal le nombre $ch s’écrit %o <br/>", $ch);
+
+$ch ="252756";
+printf ("En hexadécimal $ch s'écrit %x <br/>", $ch);
+?>
